cc072a108e4d9753ba4c382f3d9db2d0ad95a56d
Sam Shadwell <sam.shadwell@airbnb.com>
2022-01-12 13:50:59 -0800

    Add basic service/platform.

---
+++ service/src/test/java/com/example/HelloTest.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+class HelloTest {
+    @Test
+    void example() {
+        assertEquals(1, 1);
+    }
+}
